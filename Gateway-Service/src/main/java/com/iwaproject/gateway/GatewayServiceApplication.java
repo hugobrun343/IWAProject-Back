466e62ea1dbc5abf5f3d437007fc1a7ca0c67f4b
hugobrun <hugo.brun@etu.umontpellier.fr>
2025-09-26 10:08:18 +0200

    Fix: workflows

--- Gateway-Service/src/main/java/com/iwaproject/gateway/GatewayServiceApplication.java
+++ Gateway-Service/src/main/java/com/iwaproject/gateway/GatewayServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-        // test2
+        // test3
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
