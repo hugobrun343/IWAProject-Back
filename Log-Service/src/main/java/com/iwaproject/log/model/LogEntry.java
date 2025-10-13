8ae78334e4ef72a033cb820443394c25a139f655
hugobrun <hugo.brun@etu.umontpellier.fr>
2025-10-13 09:14:36 +0200

    refactor(log-service): add Lombok to LogEntry model

--- Log-Service/src/main/java/com/iwaproject/log/model/LogEntry.java
+++ Log-Service/src/main/java/com/iwaproject/log/model/LogEntry.java
@@ -1,5 +1,7 @@
 package com.iwaproject.log.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -10,6 +12,8 @@ import java.time.Instant;
 /**
  * Log entry model for Elasticsearch storage
  */
+@Data
+@NoArgsConstructor
 @Document(indexName = "microservices-logs")
 public class LogEntry {
 
@@ -26,7 +30,7 @@ public class LogEntry {
     private String message;
 
     @Field(type = FieldType.Date)
-    private Instant timestamp;
+    private Instant timestamp = Instant.now();
 
     @Field(type = FieldType.Text)
     private String exception;
@@ -39,82 +43,5 @@ public class LogEntry {
 
     @Field(type = FieldType.Object)
     private Object metadata;
-
-    public LogEntry() {
-        this.timestamp = Instant.now();
-    }
-
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getService() {
-        return service;
-    }
-
-    public void setService(String service) {
-        this.service = service;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Instant timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getException() {
-        return exception;
-    }
-
-    public void setException(String exception) {
-        this.exception = exception;
-    }
-
-    public String getLogger() {
-        return logger;
-    }
-
-    public void setLogger(String logger) {
-        this.logger = logger;
-    }
-
-    public String getThread() {
-        return thread;
-    }
-
-    public void setThread(String thread) {
-        this.thread = thread;
-    }
-
-    public Object getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(Object metadata) {
-        this.metadata = metadata;
-    }
 }
 
